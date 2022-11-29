15209ca81b8ec36fdd861b6b610913b83108aa91
Matheus Cruz de Moura <matheuscruz.moura27@gmail.com>
2022-11-28 22:29:30 -0300

    implementando a alteração e remoção

--- src/main/java/br/com/ifpe/oxefoodmatheus/servicos/produto/CategoriaProdutoController.java
+++ src/main/java/br/com/ifpe/oxefoodmatheus/servicos/produto/CategoriaProdutoController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +34,8 @@ public class CategoriaProdutoController extends GenericController {
     @PostMapping
     public ResponseEntity<CategoriaProduto> save(@RequestBody @Valid CategoriaProdutoRequest request) {
 
-	validarPreenchimentoChave(request.getChaveEmpresa());
-	CategoriaProduto categoriaProduto = categoriaProdutoService.save(request.buildCategoriaProduto());
+		validarPreenchimentoChave(request.getChaveEmpresa());
+		CategoriaProduto categoriaProduto = categoriaProdutoService.save(request.buildCategoriaProduto());
 		return new ResponseEntity<CategoriaProduto>(categoriaProduto, HttpStatus.CREATED);
     }
     
@@ -58,6 +60,22 @@ public class CategoriaProdutoController extends GenericController {
     public List<CategoriaProduto> consultarPorChaveEmpresa(@PathVariable String chaveEmpresa) {
 	
     	return categoriaProdutoService.consultarPorChaveEmpresa(chaveEmpresa);
+    }
+    
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Serviço responsável por atualizar as informações da categoria de produto no sistema.")
+    public ResponseEntity<CategoriaProduto> update(@PathVariable("id") Long id, @RequestBody CategoriaProdutoRequest request) {
+
+		categoriaProdutoService.update(id, request.buildCategoriaProduto());
+		return ResponseEntity.ok().build();
+    }
+    
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Rota responsável por remover(exclusão lógica) uma categoria de produto do sistema.")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+	 categoriaProdutoService.delete(id);
+	 return ResponseEntity.noContent().build();
     }
 
 }
